e62e1b57389eff54f76b47f82fa3c2ff2ddf994c
Francisco Vivas <franciscovivas.codes@gmail.com>
2020-12-01 16:02:13 -0500

    Init branch commit

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './components/home/Home';
-import NotFound from './components/404/NotFound.js';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import NotFound from "./components/404/NotFound.js";
+
+const Signup = () => <h1>Sign up</h1>;
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={Signup} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
