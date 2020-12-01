8f67f328205afc35cfd7bbe2b159839db3743627
Maria Reyna <mereynag@gmail.com>
2020-12-01 12:57:21 -0400

    Layout general

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './components/home/Home';
+import Layout from './components/MainLayout';
+import Home from './components/Home';
 import NotFound from './components/404/NotFound.js';
 
 const Router = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import { } from 'antd';
+
+export default function Home(){
+    return (
+        <div>
+            <h1>Main Dashboard</h1>
+        </div>
+    )
+}
