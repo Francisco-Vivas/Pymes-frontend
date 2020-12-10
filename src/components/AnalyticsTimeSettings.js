7b54e9b074c65802b18d3a90de094c3b622f8739
Francisco Vivas <franciscovivas.codes@gmail.com>
2020-12-10 14:14:28 -0500

    Add Analytics time settings

---
+++ src/components/AnalyticsTimeSettings.js
@@ -0,0 +1,98 @@
+import { Form, Select, DatePicker, Menu } from "antd";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import { ButtonS } from "./styledComponents/antdStyled";
+
+const { RangePicker } = DatePicker;
+
+const AnalyticsTimeSettings = ({ form }) => {
+  const [formState, setFormState] = useState({
+    dateRange: "last7days",
+    rangePicker: [moment().subtract(7, "days"), moment()],
+  });
+
+  useEffect(() => {
+    form.setFieldsValue(formState);
+  }, [formState]);
+
+  function handleOnFinish(value) {
+    console.log(value);
+  }
+
+  function handlerDateRangePicker(value) {
+    const rangePicker = [];
+    switch (value) {
+      case "last7days":
+        rangePicker.push(moment().subtract(7, "days"));
+        break;
+      case "last15days":
+        rangePicker.push(moment().subtract(15, "days"));
+        break;
+      case "last30days":
+        rangePicker.push(moment().subtract(30, "days"));
+        break;
+      case "lastMonth":
+        rangePicker.push(moment().subtract(1, "months").startOf("month"));
+        break;
+      case "weekToDate":
+        rangePicker.push(moment().startOf("week"));
+        break;
+      case "monthToDate":
+        rangePicker.push(moment().startOf("month"));
+        break;
+      case "quarterToDate":
+        rangePicker.push(moment().startOf("quarter"));
+        break;
+      case "yearToDate":
+        rangePicker.push(moment().startOf("year"));
+        break;
+      case "custom":
+        break;
+    }
+    if (value !== "custom") {
+      if (value !== "lastMonth") {
+        rangePicker.push(moment());
+      } else {
+        rangePicker.push(moment().subtract(1, "months").endOf("month"));
+      }
+      setFormState({ ...formState, rangePicker, dateRange: value });
+    }
+  }
+
+  function handleChangeRangePicker() {
+    setFormState({ ...formState, dateRange: "custom" });
+  }
+  return (
+    <Menu>
+      <Form
+        layout="vertical"
+        form={form}
+        onFinish={handleOnFinish}
+        initialValues={formState}
+        style={{ height: "100%", margin: "1rem" }}
+      >
+        <Form.Item name="dateRange" label="Date range">
+          <Select onSelect={handlerDateRangePicker}>
+            <Select.Option value="last7days">Last 7 days</Select.Option>
+            <Select.Option value="last15days">Last 15 days</Select.Option>
+            <Select.Option value="last30days">Last 30 days</Select.Option>
+            <Select.Option value="lastMonth">Last Month</Select.Option>
+            <Select.Option value="weekToDate">Week to date</Select.Option>
+            <Select.Option value="monthToDate">Month to date</Select.Option>
+            <Select.Option value="quarterToDate">Quarter to date</Select.Option>
+            <Select.Option value="yearToDate">Year to date</Select.Option>
+            <Select.Option value="custom">Custom</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item name="rangePicker">
+          <RangePicker onCalendarChange={handleChangeRangePicker} />
+        </Form.Item>
+        <ButtonS type="primary" htmlType="submit">
+          Apply
+        </ButtonS>
+      </Form>
+    </Menu>
+  );
+};
+
+export default AnalyticsTimeSettings;
--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
-import { Col, Row, Skeleton } from "antd";
+import { Col, Dropdown, Form, Row, Skeleton } from "antd";
 import { TitleS } from "../components/styledComponents/Typography";
 import { getAllOrders } from "../services/orders";
 import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme } from "victory";
+import AnalyticsTimeSettings from "../components/AnalyticsTimeSettings";
+import { ButtonS } from "../components/styledComponents/antdStyled";
+import Moment from "moment";
+import { extendMoment } from "moment-range";
+
+const moment = extendMoment(Moment);
 
 export default function Analytics() {
   const [orders, setOrders] = useState(null);
@@ -11,6 +17,7 @@ export default function Analytics() {
   const [dataPlot, setDataPlot] = useState([]);
   const [keys, setKeys] = useState({ one: "date", two: "quantity" });
   const [justValues, setJustValues] = useState(false);
+  const [form] = Form.useForm();
 
   function settingDataToPlot(filtered, newArr = true, baseArr = []) {
     const toData = newArr ? [] : baseArr;
@@ -66,6 +73,7 @@ export default function Analytics() {
     );
   }
 
+  /* Generates all dates between two Dates */
   function generateInBetweenersDates(date, value = 7, type = "D") {
     const arrOut = [];
     for (let i = 1; i <= value + 1; i++) {
@@ -86,6 +94,71 @@ export default function Analytics() {
     return arrOut;
   }
 
+  /* Generates all dates between two moments */
+  function generateAllDates(
+    momentStart = moment().subtract(8, "days"),
+    momentStop = moment(),
+    dateRange = "last7days"
+  ) {
+    let initValue = 0,
+      endtValue = 0,
+      formated = [];
+    const arrOut = [];
+    if (dateRange === "quarterToDate" || dateRange === "yearToDate") {
+      initValue = momentStart.month();
+      endtValue = momentStop.month();
+      formated = ["M", "MMM"];
+    } else {
+      initValue = momentStart.date();
+      endtValue = momentStop.date();
+      formated = ["d", "MMM Do"];
+    }
+    console.log(initValue, endtValue, formated[0]);
+
+    for (let i = 0; i < endtValue - initValue; i++) {
+      arrOut.push({
+        [keys.one]: momentStart.add(1, formated[0]).format(formated[1]),
+        [keys.two]: 0,
+      });
+    }
+
+    return moment("2020-12-10");
+  }
+
+  function settingMomentsToPlot(filtered, newArr = true, baseArr = []) {
+    const toData = newArr ? [] : baseArr;
+
+    if (newArr) {
+      const dataExtract = filtered.map((e) => e[keys.one]);
+      dataExtract.forEach((value, index, array) => {
+        const arrIndex = array.indexOf(value);
+        if (index === arrIndex) {
+          toData.push({ [keys.one]: value, [keys.two]: 1 });
+        } else {
+          toData[arrIndex][keys.two] += 1;
+        }
+      });
+    } else {
+      const baseKeyOne = baseArr.map((e) => e[keys.one]);
+      const dataExtract = filtered.map((e) => e[keys.one]);
+      dataExtract.forEach((value, index, array) => {
+        const arrIndex = array.indexOf(value);
+        const baseIndex = baseKeyOne.indexOf(value);
+        if (baseIndex + 1 && index === arrIndex) {
+          toData[baseIndex][keys.two] = 1;
+        } else {
+          toData[baseIndex][keys.two] += 1;
+        }
+      });
+    }
+
+    return toData;
+  }
+
+  function filterOrders(orders, sinceThisTime) {
+    return orders.filter((e) => new Date(`${e.date}T05:00`) >= sinceThisTime);
+  }
+
   useEffect(() => {
     async function getOrders() {
       const { data } = await getAllOrders();
@@ -93,15 +166,18 @@ export default function Analytics() {
       const date = new Date();
       const sinceThisTime = fromNowToWhen(date, 15, "D");
       setCurrentDate(date);
+
       const filtered = data.filter(
         (e) => new Date(`${e.date}T05:00`) >= sinceThisTime
       );
+
       setOrdersFiltered(filtered);
       const baseArr = generateInBetweenersDates(date, 15, "D");
       const toPlot = settingDataToPlot(filtered, justValues, baseArr);
       setDataPlot(toPlot);
     }
     getOrders();
+    console.log(generateAllDates());
   }, []);
 
   useEffect(() => {
@@ -120,8 +196,17 @@ export default function Analytics() {
 
   return (
     <Row justify="center" align="middle">
-      <Col xs={24} sm={24} md={20} lg={15}>
+      <Col xs={24} sm={24} md={12} lg={12} xl={12}>
         <TitleS style={{ textAlign: "left" }}>Analytics</TitleS>
+        <Dropdown
+          overlay={() => <AnalyticsTimeSettings form={form} />}
+          placement="bottomLeft"
+          trigger={["click"]}
+        >
+          <ButtonS type="primary">Config</ButtonS>
+        </Dropdown>
+      </Col>
+      <Col xs={24} sm={24} md={12} lg={12} xl={12}>
         <Col>
           {ordersFiltered ? (
             <VictoryChart domainPadding={25} theme={VictoryTheme.material}>
